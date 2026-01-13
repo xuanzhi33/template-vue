b17b49ef650d33acb5739d5ca3da713a0d9e66e4
xuanzhi33 <xuanzhi33@qq.com>
2026-01-13 00:21:41 +0000

    feat: enhance Home.vue with internationalization support and settings navigation

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,21 @@
 <script setup lang="ts">
+import { useI18n } from 'vue-i18n';
+import { useRouter } from 'vue-router';
+import { Button } from '@/components/ui/button';
+import { Settings } from 'lucide-vue-next';
 
+const { t } = useI18n()
+const router = useRouter()
 </script>
 
 <template>
   <div class="p-3">
-    Hello World
+    <h1 class="text-xl mb-3">
+      {{ t('common.title') }}
+    </h1>
+    <Button variant="outline" @click="router.push('/settings')">
+      <Settings />
+      {{ t('settings.title') }}
+    </Button>
   </div>
 </template>
